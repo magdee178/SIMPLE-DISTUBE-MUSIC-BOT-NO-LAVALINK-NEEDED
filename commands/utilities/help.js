1e50dd14592eed34957d1dafecd2c39e0ab870be
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:58:22 +0200

    Update help.js

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -13,17 +13,17 @@ module.exports = {
         accessableby: "Members"
     },
     run: async (client, message, args) => {
-        console.log(chalk.magenta(`[COMMAND] Help used by ${message.author.tag} from ${message.guild.name}`));
+        //console.log(chalk.magenta(`[COMMAND] Help used by ${message.author.tag} from ${message.guild.name}`));
         const embed = new MessageEmbed()
             .setColor('#2f3136')
-            .setAuthor({ name: `${message.guild.me.displayName} Help Command!`, iconURL: message.guild.iconURL({ dynamic: true })})
+            .setAuthor({ name: `قائمة المساعدة لـ ${message.guild.me.displayName}`, iconURL: message.guild.iconURL({ dynamic: true })})
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }));
 
         if(!args[0]) {
             const categories = readdirSync("./commands/")
 
-            embed.setDescription(`The bot prefix is: **${client.prefix}**`)
-            embed.setFooter({ text: `© ${message.guild.me.displayName} | Total Commands: ${client.commands.size}`, iconURL: client.user.displayAvatarURL({ dynamic: true })});
+            embed.setDescription(` بادئة البوت: **${client.prefix}**`)
+            embed.setFooter({ text: `تم الطلب من: ${message.guild.members.cache.get(message.author.id).displayName} | إجمالي الأوامر: ${client.commands.size}`, iconURL: client.user.displayAvatarURL({ dynamic: true })})
 
             categories.forEach(category => {
                 const dir = client.commands.filter(c => c.config.category === category)
@@ -38,15 +38,14 @@ module.exports = {
             return message.channel.send({ embeds: [embed] })
         } else {
             let command = client.commands.get(client.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
-            if(!command) return message.channel.send({ embeds: [embed.setTitle("Invalid Command.").setDescription(`Do \`${client.prefix}help\` for the list of the commands.`)] })
+            if(!command) return message.channel.send({ embeds: [embed.setTitle("الأمر غير موجود!").setDescription(`أكتب: \`${client.prefix}help\` للحصول على قائمة الأوامر.`)] })
             command = command.config
 
-            embed.setDescription(stripIndents`The client's prefix is: \`${client.prefix}\`\n
-            **Command:** ${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)}
-            **Description:** ${command.description || "No Description provided."}
-            **Usage:** ${command.usage ? `\`${client.prefix}${command.name} ${command.usage}\`` : "No Usage"}
-            **Accessible by:** ${command.accessableby || "Members"}
-            **Aliases:** ${command.aliases ? command.aliases.join(", ") : "None."}`)
+            embed.setDescription(stripIndents` بادئة البوت: \`${client.prefix}\`\n
+            **الأمر:** ${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)}
+            **الوصف:** ${command.description || "لم يتم توفير الوصف."}
+            **الأستخدام:** ${command.usage ? `\`${client.prefix}${command.name} ${command.usage}\`` : "لم يتم توفير الأستخدام."}
+            **الأختصار:** ${command.aliases ? command.aliases.join(", ") : "لا يوجد أختصارات."}`)
 
             return message.channel.send({ embeds: [embed] })
         }
