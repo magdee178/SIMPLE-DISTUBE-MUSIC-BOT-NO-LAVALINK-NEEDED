e3cafbf1c162e3976a70e643d5f7402da6816ce1
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:48:40 +0200

    Update playskip.js

--- commands/music/playskip.js
+++ commands/music/playskip.js
@@ -8,18 +8,18 @@ module.exports = {
         category: "music",
     },
     run: async (client, message, args) => {
-        message.channel.send(`**Searching.....** \`${args.join(" ")}\``).then(msg => {
+        message.channel.send(`**بحث:.....** \`${args.join(" ")}\``).then(msg => {
             setTimeout(() => msg.delete(), 5000)
         })
         
         const { channel } = message.member.voice;
-        if (!channel) return message.channel.send("You need to be in voice channel.")
+        if (!channel) return message.channel.send("يجب أن تكون في القناة الصوتية.")
         if (!message.guild.me.permissions.has([Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK])) return msg.edit({ embed: { description: "I don't have perm `CONNECT` or `SPEAK` to execute command!", color: "#2f3136" } });
         if (!message.guild.me.permissionsIn(channel).has([Permissions.FLAGS.CONNECT, Permissions.FLAGS.SPEAK])) return msg.edit({ embed : { description: `I don't have perm \`CONNECT\` or \`SPEAK\` in ${channel.name} to join voice!`, color: "#2f3136" } });
 
         const string = args.join(" ");
         if (!string) {
-            return message.channel.send("Please provide a song name or link.");
+            return message.channel.send("يرجى تقديم اسم الأغنية أو الرابط.");
         }
 
         const options = {
