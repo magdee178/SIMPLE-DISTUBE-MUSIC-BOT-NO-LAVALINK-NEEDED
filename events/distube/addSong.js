081e9c31a457abbc510b46d86f92440db28a4390
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:04:47 +0200

    Update addSong.js

--- events/distube/addSong.js
+++ events/distube/addSong.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, queue, song) => {
     let embed = new MessageEmbed()
-    .setDescription(`**Queued • [${song.name}](${song.url})** \`${song.formattedDuration}\` • ${song.user}`)
+    .setDescription(`**في قائمة الانتظار**\n**Name: [${song.name}](${song.url})**\n**المدة:** \`${song.formattedDuration}\`\n**تم الطلب من:**\n${song.user}`)
     .setColor('#2f3136')
 
     queue.textChannel.send({ content: ' ', embeds: [embed] })
