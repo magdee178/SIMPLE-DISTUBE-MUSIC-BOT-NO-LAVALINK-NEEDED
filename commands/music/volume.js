c826a30dd7bdb2737c4fb177f7461659efb025a3
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:54:50 +0200

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -9,19 +9,19 @@ module.exports = {
         category: "music",
     },
     run: async (client, message, args) => {
-        const msg = await message.channel.send("Processing.....");
+        const msg = await message.channel.send("**يعالج.....**");
 
         const queue = client.distube.getQueue(message);
-        if (!queue) msg.edit(`There is nothing in the queue right now!`)
+        if (!queue) msg.edit(`لا يوجد شيء في قائمة الانتظار الآن!`)
         const { channel } = message.member.voice;
-        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("You need to be in a same/voice channel.")
+        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("يجب أن تكون في نفس القناة/القناة الصوتية.")
 
         const volume = parseInt(args[0]);
 
         if (!volume) {
             const embed = new MessageEmbed()
                 .setColor("#2f3136")
-                .setDescription(`Current **volume** : \`${queue.volume}\`%`)
+                .setDescription(`**مستوى الصوت الحالي:** \`${queue.volume}\`%`)
 
             return msg.edit({ content: ' ', embeds: [embed] });
         }
@@ -29,18 +29,18 @@ module.exports = {
         if (isNaN(volume)) {
             const embed = new MessageEmbed()
                 .setColor("#2f3136")
-                .setDescription(`Please enter a valid number`);
+                .setDescription(`من فضلك أدخل رقما صالحا`);
 
             return msg.edit({ content: ' ', embeds: [embed] });
         }
 
-        if (Number(volume) < 1 || Number(volume) > 100) return msg.edit(`Please provide a number between 1 and 100`)
+        if (Number(volume) < 1 || Number(volume) > 100) return msg.edit(`يرجى تقديم رقم بين **1** و **100**`)
 
         client.distube.setVolume(message, volume);
 
         const embed = new MessageEmbed()
             .setColor("#2f3136")
-            .setDescription(`\`🔊\` | **Change volume to:** \`${args[0]}\`%`)
+            .setDescription(`\`🔊\` | **تم تغيير مستوى الصوت إلى:** \`${args[0]}\`%`)
 
         msg.edit({ content: ' ', embeds: [embed] });
 
