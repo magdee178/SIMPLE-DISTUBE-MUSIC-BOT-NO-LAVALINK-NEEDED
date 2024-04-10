20d70d727935e87dcc9ac3da7324b9dec5e4b220
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:00:19 +0200

    Update stopbot.js

--- commands/utilities/stopbot.js
+++ commands/utilities/stopbot.js
@@ -10,14 +10,21 @@ module.exports = {
         aliases: ["stopbot"]
     },
     run: async (client, message, args) => {
-    if(message.author.id != client.owner) return message.channel.send("You not the client the owner!")
+      const embed = new MessageEmbed()
+        .setColor("#2f3136")
+        .setDescription("`❌` | **Only the bot owner can use this command!**")
+      .setFooter({ text: `Requested by: ${message.guild.members.cache.get(message.author.id).displayName}`, iconURL: message.author.displayAvatarURL({ dynamic: true })})
+      if(message.author.id != "1150890847768936458") return message.channel.send({ embeds: [embed] }).then(msg => {
+            setTimeout(() => msg.delete(), 50000)
+        });
+    //if(message.author.id != client.owner) return message.channel.send("You not the client the owner!")
 
     const restart = new MessageEmbed()
-        .setDescription("**Account has been**: `Shutting down...`")
+        .setDescription("**الحساب الأن:** `جاري إغلاقه...`")
         .setColor("#2f3136");
 
     await message.channel.send({ embeds: [restart] });
-        console.log(chalk.red(`TEC | [CLIENT] Restarting...`));
+       console.log(chalk.red(`[CLIENT]: Restarting...`));
             
     process.exit();
     }
