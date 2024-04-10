ec5cdbbbf243d258f50459de2a8e5e393865c88d
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:07:53 +0200

    Update searchNoResult.js

--- events/distube/searchNoResult.js
+++ events/distube/searchNoResult.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = async (client, query, queue) => {
     const embed = new MessageEmbed()
         .setColor("#2f3136")
-        .setDescription(`No result found for ${query}!`)
+        .setDescription(`لم يتم العثور على نتيجة لـ: ${query}!`)
 
     queue.textChannel.send({ embeds: [embed] })
 }
