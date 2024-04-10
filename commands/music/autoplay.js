039c8bdbbd7d9a29d0e34b74013952e9f15f1707
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:37:25 +0200

    Update autoplay.js

--- commands/music/autoplay.js
+++ commands/music/autoplay.js
@@ -9,19 +9,19 @@ module.exports = {
         category: "music",
     },
     run: async (client, message, args) => {
-        const msg = await message.channel.send("Processing.....");
+        const msg = await message.channel.send("**يعالج.....**");
         
         const queue = client.distube.getQueue(message);
-        if (!queue) msg.edit(`There is nothing in the queue right now!`)
+        if (!queue) msg.edit(`**لا يوجد شيء في قائمة الانتظار الآن!**`)
         const { channel } = message.member.voice;
-        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("You need to be in a same/voice channel.")
+        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("**يجب أن تكون في نفس القناة/القناة الصوتية.**")
 
         if (!queue.autoplay) {
             client.distube.toggleAutoplay(message);
     
             const embed = new MessageEmbed()
                 .setColor(message.client.color)
-                .setDescription(`\`⏯\` Activate **Autoplay** mode.`)
+                .setDescription(`\`⏯\`** تفعيل وضع **التشغيل التلقائي**.`)
 
             msg.edit({ content: ' ', embeds: [embed] });
         } else {
@@ -29,7 +29,7 @@ module.exports = {
 
             const embed = new MessageEmbed()
                 .setColor(message.client.color)
-                .setDescription(`\`⏯\` Disable **Autoplay** mode.`)
+                .setDescription(`\`⏯\` تعطيل وضع **التشغيل التلقائي**.`)
 
             msg.edit({ content: ' ', embeds: [embed] });
         }
