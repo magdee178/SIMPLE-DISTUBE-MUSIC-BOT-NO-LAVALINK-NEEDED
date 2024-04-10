bdafd7ce65fd63ab0341c5f96eb80d7b029877fe
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:42:34 +0200

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -10,12 +10,12 @@ module.exports = {
         category: "music",
     },
     run: async (client, message, args) => {
-        const msg = await message.channel.send("Searching for lyrics...");
+        const msg = await message.channel.send("البحث عن كلمات...");
 
         const queue = client.distube.getQueue(message);
-        if (!queue) msg.edit(`There is nothing in the queue right now!`)
+        if (!queue) msg.edit(`لا يوجد شيء في قائمة الانتظار الآن!`)
         const { channel } = message.member.voice;
-        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("You need to be in a same/voice channel.")
+        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("يجب أن تكون في نفس القناة/القناة الصوتية.")
 
         let song = args.join(" ");
             let CurrentSong = queue.songs[0];
@@ -25,20 +25,20 @@ module.exports = {
 
         try {
             lyrics = await lyricsfinder(song, "");
-            if (!lyrics) msg.edit("Couldn't find any lyrics for that song!");
+            if (!lyrics) msg.edit("لم أتمكن من العثور على أي كلمات لهذه الأغنية!");
         } catch (err) {
             console.log(err);
-            msg.edit("Couldn't find any lyrics for that song!");
+            msg.edit("لم أتمكن من العثور على أي كلمات لهذه الأغنية!");
         }
         let lyricsEmbed = new MessageEmbed()
             .setColor('#2f3136')
-            .setTitle(`Lyrics`)
+            .setTitle(`كلمات`)
             .setDescription(`**${song}**\n${lyrics}`)
             .setFooter({ text: `Requested by ${message.author.username}`})
             .setTimestamp();
 
         if (lyrics.length > 2048) {
-            lyricsEmbed.setDescription("Lyrics too long to display!");
+            lyricsEmbed.setDescription("كلمات طويلة جدا للعرض!");
         }
 
         msg.edit({ content: ' ', embeds: [lyricsEmbed] })
