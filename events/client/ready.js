9e96e16e3c2d3ae875f7917c808e02b240fc0212
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:03:03 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,26 +2,31 @@ const figlet = require('figlet');
 const chalk = require('chalk');
 
 module.exports = async (client) => {
+  client.user.setStatus("idle")
   figlet(client.user.tag, function(err, data) {
     if (err) {
-        console.log('TEC | Something went wrong...');
+        console.log('MaGdEe | Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(chalk.red.bold(data));
+    //console.log(chalk.red.bold(data));
+    console.log(chalk.red.bold(`Loged in as ${client.user.tag}!`))
+    console.log(chalk.red.bold(`Bot Id: ${client.user.id}`))
+    console.log(chalk.red.bold(`Bot Prefix: ${client.config.PREFIX}`))
   });
+  console.log(chalk.red.bold(`guilds: ${client.guilds.cache.size}`))
 
   let guilds = client.guilds.cache.size;
   let users = client.users.cache.size;
   let channels = client.channels.cache.size;
 
   const activities = [
-      `${client.prefix}help | TEAM TEC `,
-      `${client.prefix}play <input> | TEAM TEC `,
-      `${client.prefix}filterlist | TEAM TEC `,
+      `${client.prefix}about`,
+      `${client.prefix}play`,
+      `${client.prefix}help`,
   ]
 
   setInterval(() => {
-      client.user.setActivity(`${activities[Math.floor(Math.random() * activities.length)]}`, { type: 'WATCHING' });
-  }, 15000)
-}
+      client.user.setActivity(`${activities[Math.floor(Math.random() * activities.length)]}`, { type: 'PLAYING' });
+  }, 10000)
+                                                       }
