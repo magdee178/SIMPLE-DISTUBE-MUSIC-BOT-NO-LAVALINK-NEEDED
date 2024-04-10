60f383234cbf6132fc78293e4af4cf77bacb1a63
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:54:23 +0200

    Update skipto.js

--- commands/music/skipto.js
+++ commands/music/skipto.js
@@ -9,17 +9,17 @@ module.exports = {
         category: "music",
     },
     run: async (client, message, args) => {
-        const msg = await message.channel.send("Processing.....");
+        const msg = await message.channel.send("**يعالج.....**");
 
         const queue = client.distube.getQueue(message);
-        if (!queue) msg.edit(`There is nothing in the queue right now!`)
+        if (!queue) msg.edit(`لا يوجد شيء في قائمة الانتظار الآن!`)
         const { channel } = message.member.voice;
-        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("You need to be in a same/voice channel.")
+        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("يجب أن تكون في نفس القناة/القناة الصوتية.")
 
         if (isNaN(args[0])) {
             const embed = new MessageEmbed()
                 .setColor("#2f3136")
-                .setDescription(`Please enter a valid number!`);
+                .setDescription(`من فضلك أدخل رقما صالحا!`);
 
             return msg.edit({ content: ' ', embeds: [embed] });
         }
@@ -28,7 +28,7 @@ module.exports = {
             .then(queue => {
                 const embed = new MessageEmbed()
                     .setColor("#2f3136")
-                    .setDescription(`\`⏭\` | **Skipto:** ${args[0]}`)
+                    .setDescription(`\`⏭\` | **انتقل إلى:** ${args[0]}`)
 
                 msg.edit({ content: ' ', embeds: [embed] });
             });
