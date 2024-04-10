16adea8c007f6f0c823533a192864b845f2484a9
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:14:09 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTIyMDA0MzAyNDU1NDUyNDY5Mg.G2wdQd.PgOVv8ZDiVD7pSywbq8hRAjmgs4sc9jzpTyAEU", 
-    PREFIX: process.env.PREFIX || ",", 
-    OWNER_ID: process.env.OWNER_ID || "1173547185758015498", 
+    TOKEN: process.env.TOKEN || "", 
+    PREFIX: process.env.PREFIX || "-", 
+    OWNER_ID: process.env.OWNER_ID || "1150890847768936458", 
     EMPTY_LEAVE: 60, 
-    LEAVE_EMPTY: true, 
-    LEAVE_FINISH: true, 
-    LEAVE_STOP: true,
+    LEAVE_EMPTY: false, 
+    LEAVE_FINISH: false, 
+    LEAVE_STOP: false,
 }
