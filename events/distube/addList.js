1acc13857773f9e2b04a4aee58eebfee7333007a
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:04:23 +0200

    Update addList.js

--- events/distube/addList.js
+++ events/distube/addList.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, queue, playlist) => {
     const embed = new MessageEmbed()
-        .setDescription(`**Queued • [${playlist.name}](${playlist.url})** \`${queue.formattedDuration}\` (${playlist.songs.length} tracks) • ${playlist.user}`)
+        .setDescription(`**في قائمة الانتظار**\n**Name: [${playlist.name}](${playlist.url})**\n**المدة:**\`${queue.formattedDuration}\`\n**تم الطلب من:**\n(${playlist.songs.length} tracks) • ${playlist.user}`)
         .setColor('#2f3136')
   
       queue.textChannel.send({ embeds: [embed] })
