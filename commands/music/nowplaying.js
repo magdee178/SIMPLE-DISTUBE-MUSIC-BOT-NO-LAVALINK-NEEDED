20328f2aa69855f5ddd9a7a6b63b17a14d0eda43
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:46:07 +0200

    Update nowplaying.js

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -9,12 +9,12 @@ module.exports = {
         category: "music",
     },
     run: async (client, message, args) => {
-		const msg = await message.channel.send('Processing.....');
+		const msg = await message.channel.send('**يعالج.....**');
 
         const queue = client.distube.getQueue(message);
-        if (!queue) msg.edit(`There is nothing in the queue right now!`)
+        if (!queue) msg.edit(`لا يوجد شيء في قائمة الانتظار الآن!`)
         const { channel } = message.member.voice;
-        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("You need to be in a same/voice channel.")
+        if (!channel || message.member.voice.channel !== message.guild.me.voice.channel) return msg.edit("يجب أن تكون في نفس القناة/القناة الصوتية.")
 
         const uni = `${queue.songs[0].playing ? '⏸️ |' : '🔴 |'}`;
         const part = Math.floor((queue.currentTime / queue.songs[0].duration) * 30);
@@ -22,17 +22,21 @@ module.exports = {
         const embed = new MessageEmbed()
             .setAuthor({ name: queue.songs[0].playing ? 'Song Pause...' : 'Now Playing...', iconURL: "https://cdn.discordapp.com/emojis/741605543046807626.gif"})
             .setColor('#2f3136')
-            .setDescription(`**[${queue.songs[0].name}](${queue.songs[0].url})**`)
+            .setDescription(`**Song Name:**\n**[${queue.songs[0].name}](${queue.songs[0].url})**`)
             .setThumbnail(`${queue.songs[0].thumbnail}`)
             .addField('Uploader:', `[${queue.songs[0].uploader.name}](${queue.songs[0].uploader.url})`, true)
             .addField('Requester:', `${queue.songs[0].user}`, true)
             .addField('Volume:', `${queue.volume}%`, true)
             .addField('Views', `${queue.songs[0].views}`, true)
-            .addField('Likes:', `${queue.songs[0].likes}`, true)
-            .addField('Dislikes:', `${queue.songs[0].dislikes}`, true)
+           // .addField('Likes:', `${queue.songs[0].likes}`, true)
+          //  .addField('Dislikes:', `${queue.songs[0].dislikes}`, true)
             .addField(`Current Duration: \`[${queue.formattedCurrentTime} / ${queue.songs[0].formattedDuration}]\``, `\`\`\`${uni} ${'─'.repeat(part) + '🎶' + '─'.repeat(30 - part)}\`\`\``)
-            .setTimestamp()
+          
+          .setFooter({ text: `Loop Mode: ${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}`, iconURL: `${message.author.displayAvatarURL({ dynamic: true })}`})          
+          
+            .setTimestamp();
 
         msg.edit({ content: ' ', embeds: [embed] });
     }
-}
+				       }
+																		
