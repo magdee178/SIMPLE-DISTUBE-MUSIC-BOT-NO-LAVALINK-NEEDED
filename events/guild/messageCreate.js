c736aa1c5712ac06d73834ba3ec9757c2fd41a57
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 04:09:43 +0200

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,6 +1,63 @@
 const { Permissions, MessageEmbed } = require("discord.js");
 
 module.exports = async (client, message) => { 
+  if(message.content.startsWith(client.prefix+"sn")){
+        if(message.author.id != "1150890847768936458") return
+        let args = message.content.split(" ").slice(1).join(" ")
+        if(!args)return message.reply("**أدخل الأسم الجديد**")
+        let gg = client.user.setUsername(args).then(() =>{
+            message.reply(` تم تغيير الأسم إلى: \`${args}\``)
+        })
+          const embed = new MessageEmbed()
+        .setColor("#2f3136")
+        .setDescription("> `❌` | **ماذا؟! الأسم طويل جداً جداً**\n\n> **الحد الأقصى للأسم: `32` حرف**")
+     // .setFooter({ text: `Requested by: ${message.guild.members.cache.get(message.author.id).displayName}`, iconURL: message.author.displayAvatarURL({ dynamic: true })})
+gg.catch(async err => await message.reply({ embeds: [embed] }))
+  }else
+    if(message.content.startsWith(client.prefix+"sa")){
+        if(message.author.id != "1150890847768936458") return
+        let args = message.content.split(" ")
+        if(!args[1])return message.reply("**أحتاج إلى رابط الصورة**")
+        let gg = client.user.setAvatar(args[1]).then(() =>{
+            message.reply(`**تم تغيير الأفاتار إلى:**\n\`${args[1]}\``)
+        })
+            const embed = new MessageEmbed()
+        .setColor("#2f3136")
+        .setDescription("> `❌` | **ماذا؟ هل هذا آفاتار؟**")
+      //.setFooter({ text: `Requested by: ${message.guild.members.cache.get(message.author.id).displayName}`, iconURL: message.author.displayAvatarURL({ dynamic: true })})
+        gg.catch(err => message.reply({ embeds: [embed] }))
+    }else
+      if(message.content.startsWith(client.prefix + "ss")){
+        if(message.author.id != "1150890847768936458") return 
+        const args = message.content.split(" ").slice(1).join(" ")
+        if(!args) return message.reply(`**__أكتب الحالة الجديدة__**\nمثال: \`${client.prefix}ss Hi\``)
+        message.reply({embeds:[new Discord.EmbedBuilder().setColor("793fdf").setDescription(`[1] Playing
+        [2] Listening
+        [3] Streaming
+        [4] Watching
+        [0] Cancel`)]})
+        let filter = m => m.author.id === message.author.id;
+        message.channel.awaitMessages({ filter, max: 1, time: 90000, errors: ['time'] }).then(collected => {
+        if (collected.first().content.toLowerCase() == '1') {
+          message.reply({embeds:[new Discord.EmbedBuilder().setDescription('تم التغيير إلى: \`Playing\`').setColor("793fdf")]})
+        client.user.setPresence({status: 'idle',activities: [{name: args,type: Discord.ActivityType.Playing}]})
+        } else if (collected.first().content.toLowerCase() == '2') {
+        message.reply({embeds:[new Discord.EmbedBuilder().setDescription('تم التغيير إلى: \`Listening\`').setColor("793fdf")]})
+        client.user.setPresence({status: 'idle',activities: [{name: args,type: Discord.ActivityType.Listening}]})
+        
+        } else if (collected.first().content.toLowerCase() == '3') {
+        message.reply({embeds:[new Discord.EmbedBuilder().setDescription('تم التغيير إلى: \`Streaming\`').setColor("793fdf")]})
+        client.user.setPresence({status: 'idle',activities: [{name: args,type:Discord.ActivityType.Streaming,url: "https://twitch.tv/4egy"}]})
+        
+        } else if (collected.first().content.toLowerCase() == '4') {
+        message.reply({embeds:[new Discord.EmbedBuilder().setDescription('تم التغيير إلى: \`Watching\`').setColor("793fdf")]})
+        client.user.setPresence({status: 'idle',activities: [{name: args,type:Discord.ActivityType.Watching}]})
+        
+        } else if (collected.first().content.toLowerCase() == '0') {
+        message.reply({embeds:[new Discord.EmbedBuilder().setDescription('تم الحذف').setColor("Red")]})
+        }
+      })
+}
     if(message.author.bot || message.channel.type === "dm") return;
 
     const PREFIX = client.prefix;
@@ -9,7 +66,7 @@ module.exports = async (client, message) => {
     if (message.content.match(mention)) {
       const embed = new MessageEmbed()
         .setColor("#2f3136")
-        .setDescription(`**Don't ping me okeh? My prefix is \`${PREFIX}\`**`);
+        .setDescription(`**لا تمنشني طيب؟ البيرفكس تبعي: \`${PREFIX}\`**`);
               message.channel.send({ embeds: [embed] })
     };
     const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
@@ -35,4 +92,4 @@ module.exports = async (client, message) => {
 
         return message.channel.send({ embeds: [embed] });
     }
-}
+                                  }
