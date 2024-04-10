710e8410990a9e5c4a93f1a4bf1862ea9b8477ba
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-04-10 03:59:17 +0200

    Update invite.js

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -2,26 +2,27 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
     config: {
-        name: "invite",
-        aliases: [],
+        name: "about",
+        aliases: ['ab'],
+      accessableby: "Members",
         category: "utilities",
-        description: "Invite the bot to your server.",
-        accessableby: "Members"
+        description: "Information about the bot owner."
     },
     run: async (client, message, args) => {
         const embed = new MessageEmbed()
         .setColor("#2f3136")
-        .setAuthor({ name: "Invite!"})
-        .setDescription("```Invite me to your server!```")
+        .setAuthor({ name: "Owner The Bot", iconURL: client.user.displayAvatarURL({ dynamic: true })})
+        .setDescription(`**__Name:__** [MaGdEe    ,](https://discord.com/users/1150890847768936458)\n**__From:__** [Egypt](https://youtu.be/BapSQFJPMM0?si=ZFcfSNLn3RMdZdbw)\n**__Age:__** \`21\`\nUptime Bot: <t:${Math.round(client.readyTimestamp / 1000)}:R>\nPing Bot: \`${client.ws.ping}ms\`\nBot Creation Time: <t:${Math.round(client.user.createdTimestamp / 1000)}:R>\n\n**👑 Form about the Owner The Bot 👑**\n**                      👇 Click Down 👇**`)
+          .setImage("https://cdn.discordapp.com/attachments/1220651731503812649/1227026634838900746/YY52ytm.gif?ex=6626e89a&is=6614739a&hm=593a1dee0b455a53f9cec85cf5a379516708fdb885c646a397d6ab4299194c1d&")
         .setTimestamp()
-        .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL()});
+        .setFooter({ text: `Made By: MaGdEe`, iconURL: message.guild.iconURL({ dynamic: true})})
 
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
-                    .setLabel("Invite")
-                    .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot`)
-                    .setEmoji("🔗")
+                    .setLabel("bio.link")
+                    .setURL(`https://bio.link/magdee`)
+                    .setEmoji("💥")
                     .setStyle("LINK")
             )
         
